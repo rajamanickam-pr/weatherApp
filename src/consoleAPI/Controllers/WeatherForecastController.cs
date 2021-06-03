694201db25bbc37d74636d569365ebdda642034e
Rajamanickam Ramasamy <rajamanickam.rp@outlook.com>
2021-06-03 13:13:16 +0530

    Initial commit

---
+++ src/consoleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace consoleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherLib.WeatherForecast> Get()
+        {
+            WeatherLib.Weather weather = new WeatherLib.Weather();
+            return weather.GetWeather();
+        }
+    }
+}
